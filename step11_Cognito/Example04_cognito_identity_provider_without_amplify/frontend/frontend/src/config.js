cec9187f0f2252d659142fea9ed2a0d3c6209603
Hassan Sattar <hasansattar650@gmail.com>
2024-05-21 20:57:40 +0500

    Example04_cognito_identity_provider_without_amplify

---
+++ step11_Cognito/Example04_cognito_identity_provider_without_amplify/frontend/frontend/src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  domainUrl: "https://hasan-awesome-app.auth.us-east-1.amazoncognito.com",
+  clientId: "4uk6v06j3u61scp1ahepiajl4o",
+  loginRedirectUri: "http://localhost:8000/dashboard",
+  grant_type: "authorization_code",
+  logoutUri: "http://localhost:8000",
+  clientSecret: "1vhcg6nkp2kdtjblppt5tdtv7sqp6kdptdkd7b1lvh7qgr1s3ppk",
+}
+
+export default config
