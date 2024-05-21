f3dbdbf4716998a51b4ba992a59d65225e945241
Hassan Sattar <hasansattar650@gmail.com>
2024-05-21 11:43:55 +0500

    aws cognito

---
+++ step11_Cognito/Example00_define_Userpool/lib/example00_define_userpool-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+
+
+export class Example00DefineUserpoolStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+   // The code that defines your stack goes here
+
+   const userPool = new cognito.UserPool(this, "UserPool", {
+    selfSignUpEnabled: true, // Allow users to sign up
+    autoVerify: { email: true }, // Verify email addresses by sending a verification code
+    signInAliases: { email: true }, // Set email as an alias
+    userVerification: {
+      emailSubject: 'Verify your email for our awesome app!',
+      emailBody: 'Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}',
+      emailStyle: cognito.VerificationEmailStyle.CODE,
+      smsMessage: 'Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}',
+    },                                ///customize email and sms
+    standardAttributes: {
+      fullname: {
+        required: true,
+        mutable: false,
+      },
+    },                                ////Attributes already define by cognito 
+    customAttributes: {
+      'myappid': new cognito.StringAttribute({ minLen: 5, maxLen: 15, mutable: false }),
+    },                                ////Custom Attributes defined according to the application needs
+    passwordPolicy: {
+      minLength: 12,
+      requireLowercase: true,
+      requireUppercase: true,
+      requireDigits: true,
+      requireSymbols: true,
+      tempPasswordValidity: cdk.Duration.days(3),
+    },                                                        ///Password Policy
+    accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,      ///Account Recovery email
+  });
+  }
+}
