570de5018a137538f012c2d45412dd43044ec402
Hassan Sattar <hasansattar650@gmail.com>
2024-05-31 11:15:08 +0500

    step12_grant_iam_policy_to_resources

---
+++ step12_grant_iam_policy_to_resources/lib/step12_grant_iam_policy_to_resources-stack.ts
@@ -0,0 +1,94 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+ import * as appsync from 'aws-cdk-lib/aws-appsync';
+ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+ import * as lambda from 'aws-cdk-lib/aws-lambda';
+ import {Effect,PolicyStatement,Role,ServicePrincipal }  from 'aws-cdk-lib/aws-iam';
+ 
+
+export class Step12GrantIamPolicyToResourcesStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    const api = new appsync.GraphqlApi(this, "GRAPHQL_API", {
+      name: 'cdk-api',
+      schema: appsync.SchemaFile.fromAsset('graphql/schema.gql'),       ///Path specified for lambda
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,     ///Defining Authorization Type
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365))   ///set expiration for API Key
+          }
+        },
+      },
+      xrayEnabled: true                                             ///Enables xray debugging
+    })
+
+    ///Print Graphql Api Url on console after deploy
+    new cdk.CfnOutput(this, "APIGraphQlURL", {
+      value: api.graphqlUrl
+    })
+
+    ///Print API Key on console after deploy
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || ''
+    });
+
+
+    ///Defining a DynamoDB Table
+    const dynamoDBTable = new dynamodb.Table(this, 'Table', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    ///create a specific role for Lambda function
+    const role = new Role(this, 'LambdaRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    ///Attaching DynamoDb access to policy
+    const policy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['dynamodb:*', "logs:*"],
+      resources: ['*']
+    });
+
+    //granting IAM permissions to role
+    role.addToPolicy(policy);
+
+    ///Lambda Fucntion
+    const lambda_function = new lambda.Function(this, "LambdaFucntion", {
+      runtime: lambda.Runtime.NODEJS_16_X,            ///set nodejs runtime environment
+      code: lambda.Code.fromAsset("lambda"),          ///path for lambda function directory
+      handler: 'index.handler',                       ///specfic fucntion in specific file
+      timeout: cdk.Duration.seconds(10),              ///Time for function to break. limit upto 15 mins
+      role: role,                                     ///Defining role to Lambda
+      environment : {                                 ///Setting Environment Variables
+        "TABLE": dynamoDBTable.tableName
+      },
+    })
+
+    const lambda_data_source = api.addLambdaDataSource("LamdaDataSource", lambda_function);
+
+    ///Resolver mapping template reference for Lambda is also being used in it it will customize the way you want the data in your lambda function
+    ////NOTE: No need to write response Mapping Template for it if you also want to customize the response then you can write response Mapping Template.
+    lambda_data_source.createResolver("createResolverMutationcreateData",{
+      typeName: "Mutation",
+      fieldName: "createData",
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+          $util.qr($context.arguments.put("id", $util.defaultIfNull($ctx.arguments.id, $util.autoId())))
+        {
+          "version": "2017-02-28",
+          "operation": "Invoke",
+          "payload": {
+              "arguments": $util.toJson($context.arguments)
+          }
+        }
+      `)
+    })
+  }
+}
