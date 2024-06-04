ef30df4c4d2952e8860e8f3fd0628bf93f5b7c17
Hassan Sattar <hasansattar650@gmail.com>
2024-06-04 19:43:53 +0500

    step18_simple_queue_service

---
+++ step18_simple_queue_service/step0_basic_example/test/step0_basic_example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step0BasicExample from '../lib/step0_basic_example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step0_basic_example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step0BasicExample.Step0BasicExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
