8bfef68b0f40331076a185cb5eb83149ca2543d9
Hassan Sattar <hasansattar650@gmail.com>
2024-06-05 20:45:22 +0500

    step03_choice_part2

---
+++ step20_step_functions/step03_choice_part2/test/step03_choice_part2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step03ChoicePart2 from '../lib/step03_choice_part2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step03_choice_part2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step03ChoicePart2.Step03ChoicePart2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
