6c9872101e9bc0544e419f6fdddb50800286bee7
Hassan Sattar <hasansattar650@gmail.com>
2024-06-05 22:54:19 +0500

    step05_map

---
+++ step20_step_functions/step05_map/test/step05_map.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step05Map from '../lib/step05_map-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step05_map-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step05Map.Step05MapStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
