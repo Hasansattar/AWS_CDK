6f72e90367f6731f27e1a9dcb444a01a326d12a5
Hassan Sattar <hasansattar650@gmail.com>
2024-06-05 23:07:20 +0500

    step06_invoke_step_function_with_event

---
+++ step20_step_functions/step06_invoke_step_function_with_event/test/step06_invoke_step_function_with_event.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step06InvokeStepFunctionWithEvent from '../lib/step06_invoke_step_function_with_event-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step06_invoke_step_function_with_event-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step06InvokeStepFunctionWithEvent.Step06InvokeStepFunctionWithEventStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
