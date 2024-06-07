484e952d35f00a8eabbb26037c9ad82d15b18943
Hassan Sattar <hasansattar650@gmail.com>
2024-06-07 14:03:00 +0500

    step02_core_concepts

---
+++ step23_x-ray/step02_core_concepts/test/step02_core_concepts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step02CoreConcepts from '../lib/step02_core_concepts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step02_core_concepts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step02CoreConcepts.Step02CoreConceptsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
