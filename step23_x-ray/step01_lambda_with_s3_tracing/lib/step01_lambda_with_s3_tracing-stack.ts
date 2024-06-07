e960e7e2ec2e379fd8d8d28635694ffc19a8cd19
Hassan Sattar <hasansattar650@gmail.com>
2024-06-07 13:46:09 +0500

    step01_lambda_with_s3_tracing

---
+++ step23_x-ray/step01_lambda_with_s3_tracing/lib/step01_lambda_with_s3_tracing-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class Step01LambdaWithS3TracingStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    ///create a specific role for Lambda function
+    const role = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    ///Attaching s3 read only access to policy
+    const policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["s3:Get*", "s3:List*"],
+      resources: ["*"],
+    });
+
+    //granting IAM permissions to role
+    role.addToPolicy(policy);
+
+    new lambda.Function(this, "lambda-s3-x-ray-tracing", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "app.handler",
+      // Enabling X-Ray Tracing
+      tracing: lambda.Tracing.ACTIVE,
+      role: role,
+    });
+  }
+}
---
+++ step23_x-ray/step01_lambda_with_s3_tracing/test/step01_lambda_with_s3_tracing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step01LambdaWithS3Tracing from '../lib/step01_lambda_with_s3_tracing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step01_lambda_with_s3_tracing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step01LambdaWithS3Tracing.Step01LambdaWithS3TracingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
