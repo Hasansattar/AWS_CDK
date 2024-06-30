56f375de5c0ada962ecb6708874574e67e7fc781
Hassan Sattar <hasansattar650@gmail.com>
2024-07-01 03:30:14 +0500

    step32_elastic_file_storage

---
+++ step32_elastic_file_storage/test/step32_elastic_file_storage.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step32ElasticFileStorage from '../lib/step32_elastic_file_storage-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step32_elastic_file_storage-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step32ElasticFileStorage.Step32ElasticFileStorageStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
