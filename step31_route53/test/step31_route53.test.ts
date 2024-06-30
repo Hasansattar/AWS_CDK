d6b75c8c52aeb3f475c42066ad6a5949456c9ab9
Hassan Sattar <hasansattar650@gmail.com>
2024-07-01 03:29:29 +0500

    step31_route53

---
+++ step31_route53/test/step31_route53.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step31Route53 from '../lib/step31_route53-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step31_route53-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step31Route53.Step31Route53Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
