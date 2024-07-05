bad14243596449a75cf366b24d766563dd7e60c3
Hassan Sattar <hasansattar650@gmail.com>
2024-07-06 01:46:46 +0500

    step46_fargate

---
+++ step46_fargate/fargate_with_local_image_01/test/fargate_with_local_image_01.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FargateWithLocalImage01 from '../lib/fargate_with_local_image_01-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fargate_with_local_image_01-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FargateWithLocalImage01.FargateWithLocalImage01Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
