458cfe6636d6e35947da6864f2dea2d29d673668
Hassan Sattar <hasansattar650@gmail.com>
2024-07-05 18:01:43 +0500

    step38_aurora_serverless

---
+++ step38_aurora_serverless/test/step38_aurora_serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step38AuroraServerless from '../lib/step38_aurora_serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step38_aurora_serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step38AuroraServerless.Step38AuroraServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
