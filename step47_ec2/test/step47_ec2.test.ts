32ba3d1d0e05a11cc7d6a8f96f63039cf67de106
Hassan Sattar <hasansattar650@gmail.com>
2024-07-07 14:30:45 +0500

    step47_ec2

---
+++ step47_ec2/test/step47_ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step47Ec2 from '../lib/step47_ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step47_ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step47Ec2.Step47Ec2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
